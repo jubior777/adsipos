75989dd1ee05da5035195f8241cc6d42655cf2e9
jorge viloria <82420724+jubior777@users.noreply.github.com>
2022-11-15 19:49:50 -0500

    Update Constans.php

--- Controller/Constans.php
+++ Controller/Constans.php
@@ -1,6 +1,6 @@
 <?php
-if (!defined('URL_VIEWS')) define('URL_VIEWS', 'http://localhost/adsipos2/Views/');
-if (!defined('URL_CONTROLLER')) define('URL_CONTROLLER', 'http://localhost/adsipos2/Controller/');
+if (!defined('URL_VIEWS')) define('URL_VIEWS', 'http://localhost/adsipos/Views/');
+if (!defined('URL_CONTROLLER')) define('URL_CONTROLLER', 'http://localhost/adsipos/Controller/');
 if (!defined('ADDRESS')) define('ADDRESS', 'C:/xampp/htdocs/adsipos2/Views/fotoproducto/');
 ?>
 
